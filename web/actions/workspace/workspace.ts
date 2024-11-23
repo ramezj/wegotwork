4bcf5bc6e2e9be1de8c07e8d5ed651234235aade
daddylunix <ramezjoseph8@gmail.com>
2024-11-23 03:32:47 +0200

    changed the query to a way simpler, more efficient query.

--- web/actions/workspace/workspace.ts
+++ web/actions/workspace/workspace.ts
@@ -7,17 +7,32 @@ export async function GetWorkspace(slug: string) {
     const session = await auth();
     if(!session) { redirect('/') }
     try {
-        const workspace = await prisma.workspace.findFirst({
+        // const workspace = await prisma.workspace.findFirst({
+        //     where: {
+        //         slug: slug,
+        //         users: {
+        //             some: {
+        //                 userId: session.user?.id
+        //             }
+        //         }
+        //     },
+        //     include: {
+        //         users: {
+        //             select: {
+        //                 role: true
+        //             }
+        //         }
+        //     },
+        // })
+        const workspace = await prisma.workspaceUser.findFirst({
             where: {
-                slug: slug,
-                users: {
-                    some: {
-                        userId: session.user?.id
-                    }
+                userId: session.user?.id,
+                workspace: {
+                    slug: slug
                 }
             },
             include: {
-                users: true
+                workspace: true
             }
         })
         return {
