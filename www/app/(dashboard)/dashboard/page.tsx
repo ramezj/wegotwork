56f05d2ed29a9d275011bd57d3a69a3a714dd5f2
daddylunix <ramezjoseph8@gmail.com>
2024-11-25 10:52:23 +0200

    create-workspace, dialog, baby steps.

--- www/app/(dashboard)/dashboard/page.tsx
+++ www/app/(dashboard)/dashboard/page.tsx
@@ -4,8 +4,10 @@ import { redirect } from "next/navigation";
 import { UserWorkspaces } from "@/actions/workspace/user-workspaces";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { CreateWorkspaceForm } from "@/components/create-workspace";
-import { Card, CardHeader, CardContent, CardDescription, CardTitle } from "@/components/ui/card";
+import { CreateWorkspaceButton } from "@/components/create-workspace";
+import { Card, CardHeader, CardContent, CardDescription, CardTitle, CardFooter } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { DeleteWorkspace } from "@/actions/workspace/delete-workspace";
 import { DeleteWorkspaceButton } from "@/components/delete-workspace-button";
 
@@ -18,28 +20,39 @@ export default async function Page() {
         <div className="flex items-center justify-center">
         <h1 className="font-bold text-3xl">Pick a Workspace</h1>
         </div>
-        <CreateWorkspaceForm />
-        <div className="flex flex-col space-y-4">
-        {
-            userWorkspaces?.UserWorkspaces?.map((workspace) => {
-                return (
-                    <Card className="" key={workspace.workspace.id}>
-                        <CardHeader>
-                            <CardTitle>{workspace.workspace.name}</CardTitle>
-                            <CardDescription>{workspace.role}</CardDescription>
-                        </CardHeader>
-                        <CardContent>
-                        <Button asChild>
-                            <Link href={workspace.workspace.slug}>
-                            View
-                            </Link>
-                        </Button>
-                        <DeleteWorkspaceButton workspaceId={workspace.workspace.id}/>
-                        </CardContent>
-                    </Card>
-                )
-            })
-        }
+        <div className="w-full flex items-center justify-center mt-8">
+    <Card className="w-[350px]">
+      <CardHeader>
+        <CardTitle>Workspaces</CardTitle>
+        <CardDescription>Workspaces you own or are a part of</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+            {
+                userWorkspaces?.UserWorkspaces?.map((workspace) => {
+                    return (
+                        <>
+                        <div className="w-full flex border border-foreground/20 hover:border-foreground/30 rounded-md items-center duration-300" key={workspace.workspace.id}>
+                        <div className="mx-5 my-3 flex flex-col items-start text-left">
+                        <p className='text-sm font-bold text-left text-foreground'>
+                        {workspace.workspace.name}     
+                        </p>   
+                        </div>
+                        </div>
+                        </>
+                    )
+                })
+            }
+            </div>
+          </div>
+        </form>
+      </CardContent>
+        <CardFooter className="flex justify-between">
+            <CreateWorkspaceButton />
+        </CardFooter>
+        </Card>
         </div>
         </div>
     )
--- www/components/create-workspace.tsx
+++ www/components/create-workspace.tsx
@@ -3,17 +3,51 @@ import { Button } from "./ui/button"
 import { Input } from "./ui/input"
 import { useState } from "react"
 import { CreateWorkspace } from "@/actions/workspace/create"
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+  } from "@/components/ui/dialog"
+  
 
-export function CreateWorkspaceForm() {
+export function CreateWorkspaceButton() {
     const [ name, setName ] = useState<string>("");
+    const [ loading, setLoading ] = useState<boolean>(false);
+    const create_workspace = async (e:React.FormEvent) => {
+        e.preventDefault();
+        setLoading(true);
+        const create = await CreateWorkspace(name);
+        setLoading(false);
+    }
     return (
-        <div className="">
-        <form onSubmit={(() => {
-            CreateWorkspace(name);
-        })}>
-            <Input value={name} onChange={((e) => {setName(e.target.value)})} className="w-1/2"/>
-            <Button type="submit">Submit</Button>
+        <>
+        <Dialog>
+        <DialogTrigger asChild>
+        <Button variant={"outline"} className="w-full">
+        Create Workspace
+        </Button>    
+        </DialogTrigger>
+        <DialogContent>
+        <DialogHeader>
+            <DialogTitle>Create Workspace</DialogTitle>
+        </DialogHeader>
+        <form className="flex items-center gap-3" onSubmit={create_workspace}>
+        <Input required value={name} onChange={((e) => {setName(e.target.value)})} className="flex-1 w-full"/>
+        <div>
+        <Button type="submit">
+            {
+                loading
+                ? "Loading"
+                : "Create"
+            }
+        </Button>
+        </div>    
         </form>
-        </div>
+        </DialogContent>
+        </Dialog>
+        </>
     )
 }
