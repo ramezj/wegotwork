b3410f69695509182d7647023bf7084f8a3ac445
daddylunix <ramezjoseph8@gmail.com>
2024-11-28 15:06:08 +0200

    restricted access, billing/settings, we are going somewhere! :D

---
+++ www/app/[workspace]/settings/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+        <main className="p-2">
+        <h1 className="font-bold text-3xl">Settings</h1>
+        <p className="font-semibold text-muted-foreground">Restricted access, only owner can access this page.</p>
+        </main>
+    )
+}
