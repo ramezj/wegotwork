2fa1d26dcbee6bd3fd860c36941aac7c5e03424c
daddylunix <ramezjoseph8@gmail.com>
2024-11-28 18:07:33 +0200

    settings and billing check

--- www/components/layout/dashboard-layout.tsx
+++ www/components/layout/dashboard-layout.tsx
@@ -41,14 +41,14 @@ export default function LayoutNavigation({ children, session, params }: { childr
                 </div>
                   Applicants
                 </Link>
-                <Link href={`/${params.workspace}/billing`} className={`${path == '/billing' ? 'bg-accent text-foreground' : ' text-muted-foreground'} font-semibold flex items-center gap-3 rounded-md px-3 py-2 text-primary transition-all hover:text-primary hover:bg-accent duration-200`}>
+                <Link href={`/${params.workspace}/billing`} className={`${path.includes('/billing')? 'bg-accent text-foreground' : ' text-muted-foreground'} font-semibold flex items-center gap-3 rounded-md px-3 py-2 text-primary transition-all hover:text-primary hover:bg-accent duration-200`}>
                 <div className="border border-foreground/30 rounded-md dark:bg-black p-1 bg-white">
                 <Banknote className="h-4 w-4" />
                 </div>
                   Billing
                 </Link>
                 <Separator />
-                <Link href={`/${params.workspace}/settings`} className={`${path == '/settings' ? 'bg-accent text-foreground' : ' text-muted-foreground'} font-semibold flex items-center gap-3 rounded-md px-3 py-2 text-primary transition-all hover:text-primary hover:bg-accent duration-200`}>
+                <Link href={`/${params.workspace}/settings`} className={`${path.includes('/settings') ? 'bg-accent text-foreground' : ' text-muted-foreground'} font-semibold flex items-center gap-3 rounded-md px-3 py-2 text-primary transition-all hover:text-primary hover:bg-accent duration-200`}>
                 <div className="border border-foreground/30 rounded-md dark:bg-black p-1 bg-white">     
                 <Settings2 className="h-4 w-4" />
                 </div>
