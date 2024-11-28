3c21d54f90eff642bfc46002b945f4c0c9a02d7b
daddylunix <ramezjoseph8@gmail.com>
2024-11-28 21:34:38 +0200

    dashboard stats component

--- www/app/[workspace]/overview/page.tsx
+++ www/app/[workspace]/overview/page.tsx
@@ -1,12 +1,19 @@
 "use server"
 import { GetWorkspace } from "@/actions/workspace/workspace"
 import { redirect } from "next/navigation";
+import { Stats } from "@/components/stats";
+
 export default async function Page({ params } : { params: Promise<{ workspace: string }>}) {
-    const workspace = await GetWorkspace((await params).workspace);
-    if(workspace === null) { redirect('/') }
+    const userWorkspace = await GetWorkspace((await params).workspace);
+    if(userWorkspace === null) { redirect('/') }
     return (
         <main className="p-2">
         <h1 className="font-bold text-3xl">Overview</h1>
+        {/* {JSON.stringify(userWorkspace?.workspace)} */}
+        <div className="flex sm:flex-row flex-col gap-2 w-full">
+        <Stats title="Total Jobs" amount={10}/>
+        <Stats title="Total Applicants" amount={57}/>
+        </div>
         </main>
     )
 }
---
+++ www/components/stats.tsx
@@ -0,0 +1,23 @@
+import { DollarSign } from "lucide-react";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+
+export function Stats({title, amount} : { title: string, amount: number}) {
+    return (
+        <>
+        <Card className="w-full">
+                  <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle className="text-sm font-medium">
+                      {title}
+                    </CardTitle>
+                    <DollarSign className="size-4" />
+                  </CardHeader>
+                  <CardContent>
+                    <div className="text-2xl font-bold">{amount}</div>
+                    <p className="text-xs text-muted-foreground">
+                      +20.1% from last month
+                    </p>
+                  </CardContent>
+                </Card>
+        </>
+    )
+}
