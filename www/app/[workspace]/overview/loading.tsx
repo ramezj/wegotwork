8bb9bfee7a3cc52778ad7596f41989ef69206d7f
Ramez Joseph <ramezjoseph8@gmail.com>
2025-01-29 20:52:06 +0200

    🚀 settings logo, home logo in overview and in settings.

--- www/app/[workspace]/overview/loading.tsx
+++ www/app/[workspace]/overview/loading.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button";
 import { SquareArrowOutUpRight, ArrowUpRight } from "lucide-react";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Loader2 } from "lucide-react";
+import { Home } from "lucide-react";
 
 export const metadata: Metadata = {
     title: "Loading",
@@ -14,6 +15,9 @@ export default function Loading() {
         <>
         <div className="flex justify-between items-center w-full">
         <h1 className="font-bold text-3xl tracking-tight">Overview</h1>
+        <Button size={"sm"}>
+            <Home className="size-4" />
+        </Button>
         </div>
         <div className="w-full h-full items-center flex flex-col justify-center">
         <Loader2 className="size-8 animate-spin dark:text-white text-black" />
--- www/app/[workspace]/overview/page.tsx
+++ www/app/[workspace]/overview/page.tsx
@@ -5,11 +5,12 @@ import { TotalApplicants, TotalJobs } from "@/components/stats";
 import { Session } from "next-auth";
 import { auth } from "@/auth";
 import { Card, CardContent, CardDescription, CardHeader, CardFooter, CardTitle } from "@/components/ui/card";
-import { Settings } from "lucide-react";
+import { Home, Settings } from "lucide-react";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { SettingsCard } from "@/components/cards/settings";
 import { Workspace } from "@prisma/client";
+import { Button } from "@/components/ui/button";
 
 export default async function Page({ params } : { params: Promise<{ workspace: string }>}) {
     const session:Session | null = await auth();
@@ -18,7 +19,12 @@ export default async function Page({ params } : { params: Promise<{ workspace: s
     if(userWorkspace === null) { redirect('/') }
     return (
         <>
+        <div className="flex justify-between items-center w-full">
         <h1 className="font-bold text-3xl tracking-tight">Overview</h1>
+        <Button size={"sm"}>
+            <Home className="size-4" />
+        </Button>
+        </div>
         <div className="flex sm:flex-row flex-col gap-2 w-full">
         <TotalJobs title="Total Jobs" amount={10}/>
         <TotalApplicants title="Total Applicants" amount={57}/>
