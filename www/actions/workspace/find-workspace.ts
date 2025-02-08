d1c29c2fb17255be339d25be1389102ef4e2f3de
Ramez Joseph <ramezjoseph8@gmail.com>
2025-02-08 22:41:09 +0200

    find-workspace.ts server action, working on view workspace.

---
+++ www/actions/workspace/find-workspace.ts
@@ -0,0 +1,38 @@
+"use server"
+import prisma from "@/lib/db"
+import { redirect } from "next/navigation";
+
+export async function FindWorkspace(slug: string) {
+    try {
+        const workspace = await prisma.workspace.findFirst({
+            where: {
+                slug: slug
+            },
+            select: {
+                name: true,
+                slug: true,
+                description: true,
+                jobs: true
+            }
+        });
+        if(!workspace) {
+            return {
+                error:true,
+                workspace: null,
+                message:"Workspace Not Found"
+            }
+        }
+        return {
+            error: false,
+            workspace: workspace,
+            message:"Workspace Found"
+        }
+    } catch (error) {
+        console.error(error);
+        return {
+            error: true,
+            workspace:null,
+            message: "Something went wrong"
+        }
+    }
+}
--- www/app/view-workspace/[workspace]/page.tsx
+++ www/app/view-workspace/[workspace]/page.tsx
@@ -1,7 +1,10 @@
+import { FindWorkspace } from "@/actions/workspace/find-workspace"
+
 export default async function Page({ params } : { params: Promise<{ workspace: string }>}) {
+    const workspace = await FindWorkspace((await params).workspace);
     return (
         <>
-        Hello from {(await params).workspace}
+        Hello from {JSON.stringify(workspace.workspace)}
         </>
     )
 }
