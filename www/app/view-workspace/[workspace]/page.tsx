c049b5c692cb6a1bcb086aee22c122c027ac4731
Ramez Joseph <ramezjoseph8@gmail.com>
2025-02-09 01:18:37 +0200

    not-found.tsx

--- www/app/view-workspace/[workspace]/page.tsx
+++ www/app/view-workspace/[workspace]/page.tsx
@@ -1,7 +1,11 @@
 import { FindWorkspace } from "@/actions/workspace/find-workspace"
+import { notFound } from "next/navigation";
 
 export default async function Page({ params } : { params: Promise<{ workspace: string }>}) {
     const workspace = await FindWorkspace((await params).workspace);
+    if(workspace.error) {
+        notFound();
+    }
     return (
         <>
         Hello from {JSON.stringify(workspace.workspace)}
