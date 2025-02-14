502f57dcdc56e41e08780cb3d42aa7223187d0a3
Ramez Joseph <ramezjoseph8@gmail.com>
2025-02-14 12:25:57 +0200

    working on the manage member dialog

--- www/components/manage-member.tsx
+++ www/components/manage-member.tsx
@@ -3,6 +3,8 @@ import { Button } from "./ui/button"
 import { Dialog, DialogTrigger, DialogContent, DialogHeader, DialogTitle, DialogDescription, DialogFooter } from "./ui/dialog"
 import { Prisma } from "@prisma/client"
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar"
+import { CalendarDays } from "lucide-react"
+import { formatRole } from "@/lib/format-role"
 
 type WorkspaceUserWithUser = Prisma.WorkspaceUserGetPayload<{
     include: {
@@ -14,26 +16,39 @@ export function ManageMember({ WorkspaceUser } : { WorkspaceUser: WorkspaceUserW
     return (
         <>
         <Dialog>
-      <DialogTrigger asChild>
-      <Button variant={"outline"}>
-        Manage
-      </Button>
-      </DialogTrigger>
-      <DialogContent onOpenAutoFocus={((e) => {e.preventDefault()})} className="w-[90%] rounded-md bg-black">
+        <DialogTrigger asChild>
+        <Button variant={"outline"}>
+          Manage
+        </Button>
+        </DialogTrigger>
+        <DialogContent onOpenAutoFocus={((e) => {e.preventDefault()})} className="w-[90%] rounded-md bg-black">
         <DialogHeader>
           <DialogTitle className="">Manage {WorkspaceUser.user.name}</DialogTitle>
         </DialogHeader>
-        <Avatar>
+        <div className="grid gap-4 py-4">
+          <div className="flex items-center gap-4">
+            <Avatar className="h-16 w-16">
+              <AvatarImage src={WorkspaceUser.user.image!} alt="User Avatar" />
+              <AvatarFallback>JD</AvatarFallback>
+            </Avatar>
+            <div className="space-y-1">
+              <h4 className="text-lg font-semibold">{WorkspaceUser.user.name}</h4>
+              <p className="text-sm text-muted-foreground">{WorkspaceUser.user.email}</p>
+              <p className="text-sm font-medium">{formatRole(WorkspaceUser.role)}</p>
+            </div>
+          </div>
+        </div>
+        {/* <Avatar>
         <AvatarImage src={WorkspaceUser.user.image!} />
         <AvatarFallback>OM</AvatarFallback>
-        </Avatar>
+        </Avatar> */}
         <DialogFooter>
-        <Button variant={"destructive"}>
+        <Button variant={"destructive"} className="w-full">
           Revoke Access
         </Button>
         </DialogFooter>
-      </DialogContent>
-    </Dialog>
+        </DialogContent>
+        </Dialog>
         </>
     )
 }
