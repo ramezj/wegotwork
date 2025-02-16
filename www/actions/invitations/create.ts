d546802bce775612fa40fb3d5f13da0df76b44a2
Ramez Joseph <ramezjoseph8@gmail.com>
2025-02-16 19:03:19 +0200

    messing around with resend / react-email/components.

--- www/actions/invitations/create.ts
+++ www/actions/invitations/create.ts
@@ -2,6 +2,15 @@
 import { auth } from "@/auth";
 import prisma from "@/lib/db";
 import { redirect } from "next/navigation";
+import { resend } from "@/email/config";
+import { InviteUserEmail } from "@/email/invitation";
+import { Prisma } from "@prisma/client";
+
+type WorkspaceInviteWithWorkspace = Prisma.WorkspaceInviteGetPayload<{
+    include: {
+        workspace: true
+    }
+}>
 
 export async function CreateInvitation(workspaceId: string) {
     const session = await auth();
@@ -12,6 +21,23 @@ export async function CreateInvitation(workspaceId: string) {
                 workspaceId:workspaceId
             }
         })
+        const InvitationWithWorkspace = await prisma.workspaceInvite.findFirst({
+            where: {
+                id: invitation.id
+            },
+            include: {
+                workspace: true
+            }
+        })
+        if(invitation) {
+            const sendEmail = await resend.emails.send({
+                from: "noreply@heliup.xyz",
+                to:"ramezjoseph8@gmail.com",
+                subject:"You've Been Invited to an Organization",
+                react: InviteUserEmail({WorkspaceInvite: InvitationWithWorkspace as WorkspaceInviteWithWorkspace})
+            })
+            console.log(sendEmail);
+        }
         return {
             invitation
         }
