db9a564753dc04a4704ccc57e57f4d3c2d391364
Ramez Joseph <ramezjoseph8@gmail.com>
2025-03-08 23:36:39 +0200

    changed title

--- web/app/view-organization/[organization]/page.tsx
+++ web/app/view-organization/[organization]/page.tsx
@@ -3,6 +3,12 @@ import { notFound } from "next/navigation";
 import { ViewOrganization } from "@/components/view-organization";
 import { Prisma } from "@prisma/client";
 import { CareerNavbar } from "@/components/career-navbar";
+import { Metadata } from "next";
+
+export const metadata:Metadata = {
+    title: "We are hiring!",
+    description: "We are hiring!"
+}
 
 export default async function Page({ params } : { params: Promise<{ organization: string }>}) {
     type OrganizationWithJobs = Prisma.OrganizationGetPayload<{
