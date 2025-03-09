2dbf2e65fbf331dc49bd2590877d643589aea788
Ramez Joseph <ramezjoseph8@gmail.com>
2025-03-09 09:05:59 +0200

    trying out new colors

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={bricolageGrotesque.className} suppressHydrationWarning >
       <body
-        className={`${bricolageGrotesque.className} antialiased bg-[#7bf7f7]`}
+        className={`${bricolageGrotesque.className} antialiased bg-[#CFEED0]`}
       >
          <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
          <Toaster toastOptions={{
