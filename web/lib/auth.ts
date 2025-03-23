5012d41eb469e63ec53343ecfe7540239f6153bc
Ramez Joseph <ramezjoseph8@gmail.com>
2025-03-23 15:10:17 +0200

    changes to session age and expiresIn

--- web/lib/auth.ts
+++ web/lib/auth.ts
@@ -9,7 +9,9 @@ export const auth = betterAuth({
         cookieCache: {
             enabled: true,
             maxAge: 5 * 60
-        }
+        },
+        expiresIn: 60 * 60 * 24 * 7, // 7 days
+        updateAge: 60 * 60 * 24 // 1 day (every 1 day the session expiration is updated)
     },
     database: prismaAdapter(prisma, {
         provider: "postgresql"
