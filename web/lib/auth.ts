d087e213ba3478cf61dd25f018bd6e3984270b17
Ramez Joseph <ramezjoseph8@gmail.com>
2025-03-23 15:12:54 +0200

    auth.ts

--- web/lib/auth.ts
+++ web/lib/auth.ts
@@ -8,7 +8,7 @@ export const auth = betterAuth({
     session: {
         cookieCache: {
             enabled: true,
-            maxAge: 5 * 60
+            maxAge: 5 * 60 // lets see.
         },
         expiresIn: 60 * 60 * 24 * 7, // 7 days
         updateAge: 60 * 60 * 24 // 1 day (every 1 day the session expiration is updated)
