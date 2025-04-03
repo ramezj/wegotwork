392af17a9aa68d78f19dcb7ddefa48b29de0fdd7
Ramez Joseph <ramezjoseph8@gmail.com>
2025-04-03 23:29:46 +0200

    some font-size changes in statistics.tsx

--- web/components/statistics.tsx
+++ web/components/statistics.tsx
@@ -7,7 +7,7 @@ export function TotalJobs({title, amount} : { title: string, amount: number}) {
         <>
         <Card className="w-full bg-white rounded-none">
                   <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                    <CardTitle className="text-xl text-black font-extrabold">
+                    <CardTitle className="text-black font-extrabold">
                       {title}
                     </CardTitle>
                     <Briefcase className="size-4" />
@@ -25,7 +25,7 @@ export function TotalApplicants({title, amount} : { title: string, amount: numbe
       <>
       <Card className="w-full bg-white rounded-none">
                 <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                  <CardTitle className="text-xl font-extrabold text-black">
+                  <CardTitle className="font-extrabold text-black">
                     {title}
                   </CardTitle>
                   <Users className="size-4" />
