f3c0e7a7900730fd7c5bd663f5fa673c15d15425
Ramez Joseph <ramezjoseph8@gmail.com>
2025-04-08 09:42:03 +0200

    trying something new, form.

--- web/components/test-set-org.tsx
+++ web/components/test-set-org.tsx
@@ -15,7 +15,8 @@ type OrganizationUserWithOrganization = Prisma.OrganizationUserGetPayload<{
 
 
 export function TestSetOrganizationCard({ userOrganizations }: { userOrganizations : OrganizationUserWithOrganization[] }) {
-    const setUserOrg = async (organizationId: string) => {
+    const setUserOrg = async (e:React.FormEvent, organizationId: string) => {
+        e.preventDefault();
         console.log("Clicked!");
         const res = await SetCurrentOrganization(organizationId);
         if(res?.error ) {
@@ -25,51 +26,51 @@ export function TestSetOrganizationCard({ userOrganizations }: { userOrganizatio
         }
     }
     return (
-    // <Card className="w-[350px] bg-white border rounded-none">
-    // <CardHeader className="text-center">
-    //         <CardTitle className="text-black font-extrabold">
-    //             Organizations
-    //         </CardTitle>
-    //     <CardDescription className="text-black font-bold">
-    //         Organizations you own or are a part of
-    //     </CardDescription>
-    // </CardHeader>
-    // <CardContent>
-    //     <div className="grid w-full items-center">
-    //     <div className="flex flex-col">
-    //     {
-    //         userOrganizations.map((organization: OrganizationUserWithOrganization) => {
-    //         return (
-    //             <div key={organization.organizationId}>
-    //             <Button onClick={(() => {setUserOrg(organization.organizationId)})} variant={"outline"} className="my-2 w-full flex flex-col items-start text-left !rounded-none bg-white hover:bg-white border border-black text-black hover:text-black font-extrabold">           
-    //             {organization.organization.name}    
-    //             </Button>
-    //             </div>
-    //                 )
-    //             })
-    //         }
-    //         </div>
-    //     </div>
-    // </CardContent>
-    // <CardFooter className="flex flex-col space-y-2 justify-between">
-    //           <CreateOrganizationButton />
-    // </CardFooter>
-    // </Card>
-    <div className="flex gap-4">
-    {
-        userOrganizations.map((organization: OrganizationUserWithOrganization) => {
+    <Card className="w-[350px] bg-white border rounded-none">
+    <CardHeader className="text-center">
+            <CardTitle className="text-black font-extrabold">
+                Organizations
+            </CardTitle>
+        <CardDescription className="text-black font-bold">
+            Organizations you own or are a part of
+        </CardDescription>
+    </CardHeader>
+    <CardContent>
+        <div className="grid w-full items-center">
+        <div className="flex flex-col">
+        {
+            userOrganizations.map((organization: OrganizationUserWithOrganization) => {
             return (
-                <Card onClick={(() => {setUserOrg(organization.organizationId)})} key={organization.organizationId} className="rounded-none bg-white text-black border border-black cursor-pointer">
-                    <CardHeader>
-                        <CardTitle>{organization.organization.name}</CardTitle>
-                    </CardHeader>
-                    <CardContent>
-                        {organization.organization.description}
-                    </CardContent>
-                </Card>
-            )
-        })
-    }
-    </div>
+                <form onSubmit={((e) => {setUserOrg(e, organization.organizationId)})} key={organization.organizationId}>
+                <Button type="submit" variant={"outline"} className="my-2 w-full flex flex-col items-start text-left !rounded-none bg-white hover:bg-white border border-black text-black hover:text-black font-extrabold">           
+                {organization.organization.name}    
+                </Button>
+                </form>
+                    )
+                })
+            }
+            </div>
+        </div>
+    </CardContent>
+    <CardFooter className="flex flex-col space-y-2 justify-between">
+              <CreateOrganizationButton />
+    </CardFooter>
+    </Card>
+    // <div className="flex gap-4">
+    // {
+    //     userOrganizations.map((organization: OrganizationUserWithOrganization) => {
+    //         return (
+    //             <Card onClick={(() => {setUserOrg(organization.organizationId)})} key={organization.organizationId} className="rounded-none bg-white text-black border border-black cursor-pointer">
+    //                 <CardHeader>
+    //                     <CardTitle>{organization.organization.name}</CardTitle>
+    //                 </CardHeader>
+    //                 <CardContent>
+    //                     {organization.organization.description}
+    //                 </CardContent>
+    //             </Card>
+    //         )
+    //     })
+    // }
+    // </div>
     )
 }
