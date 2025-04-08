85099507e5ebd5a02b9d763cf8fe741861195336
Ramez Joseph <ramezjoseph8@gmail.com>
2025-04-08 12:12:35 +0200

    removed params from layout

--- web/app/dashboard/layout.tsx
+++ web/app/dashboard/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next"
 import { ReactNode } from "react"
 
-export default async function RootLayout({ children }: { children: ReactNode, params: Promise<{ organization: string}>}) {
+export default async function RootLayout({ children }: { children: ReactNode}) {
     return (
         <main suppressHydrationWarning>
           {children}
