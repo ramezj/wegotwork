24900aff47c55241c7f27dcffa04d75566f9accf
Ramez Joseph <ramezjoseph8@gmail.com>
2025-04-09 12:46:29 +0200

    working on pending members.

--- web/components/pending-invitations.tsx
+++ web/components/pending-invitations.tsx
@@ -6,11 +6,12 @@ import { toast } from "sonner";
 import { Dialog, DialogTrigger, DialogContent, DialogHeader, DialogDescription, DialogTitle, DialogFooter } from "./ui/dialog";
 import { Label } from "./ui/label";
 import { Input } from "./ui/input";
-import { Loader2, CheckCircle, Link, Copy, Check } from "lucide-react";
+import { Loader2, CheckCircle, Link, Copy, Check, MoreHorizontal } from "lucide-react";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle, CardFooter } from "./ui/card";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { OrganizationInvite } from "@prisma/client";
 import { formatRole } from "@/lib/format-role";
+import { Mail } from "lucide-react";
 
 export function PendingInvitations({ OrganizationInvites } : { OrganizationInvites: OrganizationInvite[] }) {
     return (
@@ -18,6 +19,11 @@ export function PendingInvitations({ OrganizationInvites } : { OrganizationInvit
       <Card className="w-full bg-white rounded-none">
       <CardHeader>
         <CardTitle className="text-2xl font-extrabold text-black">Pending invitations</CardTitle>
+        {
+          OrganizationInvites.length > 0
+          &&
+          <CardDescription className="text-gray-800 font-bold">{OrganizationInvites.length} invitations awaiting response </CardDescription>
+        }
       </CardHeader>
       <CardContent className="">
         {
@@ -31,7 +37,13 @@ export function PendingInvitations({ OrganizationInvites } : { OrganizationInvit
             OrganizationInvites.map((Invitation) => {
                 return (
                     <div className="flex items-center justify-between space-y-2" key={Invitation.id}>
+                    <div className='flex items-center gap-2'>
+                    <Mail className="size-4 text-black" />
                     <h1 className="font-bold text-black">{Invitation.email}</h1>
+                    </div>
+                    <div>
+                      <Button size={"icon"} className=''><MoreHorizontal className="text-black size-4" /></Button>
+                    </div>
                     </div>
                 )
             })
