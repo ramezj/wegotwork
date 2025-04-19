d30dd27e97f4229542622e6e8c88bb70a92f545a
Ramez Joseph <ramezjoseph8@gmail.com>
2025-04-19 09:09:13 +0200

    added metadata to /auth

--- web/app/auth/page.tsx
+++ web/app/auth/page.tsx
@@ -4,6 +4,12 @@ import { headers } from "next/headers";
 import { Navigation } from "@/components/navbar";
 import { Navbar } from "@/components/navbar-2";
 import SignIn from "@/components/sign-in";
+import { Metadata } from "next";
+
+export const metadata:Metadata = {
+  title: "Login to wegotwork",
+  description: "Login to wegotwork."
+}
 
 export default async function Home() {
   const session = await auth.api.getSession({
