d2cedda9eee4f3008005c037a11159acd898475a
Ramez Joseph <ramezjoseph8@gmail.com>
2025-04-28 10:38:35 +0300

    fixed overflowing buttons.

--- web/components/ui/custom-buttons.tsx
+++ web/components/ui/custom-buttons.tsx
@@ -6,7 +6,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "relative inline-flex items-center justify-center whitespace-nowrap rounded-md font-medium transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 active:translate-y-1 active:shadow-none text-center w-full py-3 px-4",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md font-medium transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 active:translate-y-1 active:shadow-none text-center w-full py-3 px-4",
   {
     variants: {
       variant: {
