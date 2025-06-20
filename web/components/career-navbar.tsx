08d92495f87e3bd4fce8dc66a238ea7f1d100cb4
Ramez Joseph <ramezjoseph8@gmail.com>
2025-06-20 11:23:20 +0300

    career navbar changes

--- web/components/career-navbar.tsx
+++ web/components/career-navbar.tsx
@@ -44,10 +44,10 @@ export function CareerNavbar({ organization } : { organization: OrganizationWith
     }
   }, [isOpen])
   return (
-    <div className="border-b h-16 border-white/20 bg-black sticky top-0">
+    <div className="border-b h-16 bg-black sticky top-0">
       <div className="flex h-16 items-center justify-between">
         <Link href="/" className="flex items-center z-50 pl-4">
-          <span className="text-2xl font-extrabold tracking-tighter text-white">{organization.name}</span>
+          <span className="text-2xl font-bold tracking-tighter text-white">{organization.name}</span>
         </Link>
         <div className="">
         <Button asChild variant="default" className="border-l border-b border-white/20 h-16 rounded-none text-base">
