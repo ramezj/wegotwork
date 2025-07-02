b3d01648c3449df7ef5e90973d4f6e9b562d42f4
Ramez Joseph <ramezjoseph8@gmail.com>
2025-07-02 10:42:21 +0300

    working on the invitation page

--- web/actions/invitations/get-invitation.ts
+++ web/actions/invitations/get-invitation.ts
@@ -1,43 +1,50 @@
-// "use server"
-// import { auth } from "@/auth";
-// import prisma from "@/lib/db";
-// import { Session } from "next-auth";
-// import { redirect } from "next/navigation";
+"use server"
+import { auth } from "@/lib/auth";
+import prisma from "@/lib/prisma";
+import { redirect } from "next/navigation";
+import { resend } from "@/email/config";
+import { InviteUserEmail } from "@/email/invitation";
+import { Prisma } from "@prisma/client";
+import { headers } from "next/headers";
+import { revalidatePath } from "next/cache";
 
-// export async function GetInvitation(invitationId: string) {
-//     const session:Session | null = await auth();
-//     try {
-//         const invitation = await prisma.workspaceInvite.findFirst({
-//             where: {
-//                 id: invitationId,
-//             },
-//             include: {
-//                 workspace: true
-//             }
-//         })
-//         if(!invitation) {
-//             return {
-//                 error:true,
-//                 message:"Invitation Expired or doesn't exist"
-//             }
-//         }
-//         const userAlreadyMember = await prisma.workspaceUser.findFirst({
-//             where: {
-//                 userId: session?.user?.id,
-//                 workspaceId: invitation.workspaceId
-//             }
-//         })
-//         if(userAlreadyMember) {
-//             return {
-//                 error: true,
-//                 message:"You are already a member of this organization."
-//             }
-//         }
-//         return { 
-//             error:false,
-//             invitation: invitation
-//          }
-//     } catch (error) {
-//         console.error(error);
-//     }
-// }
+export async function GetInvitation(invitationId: string) {
+    // const session = await auth.api.getSession({
+    //     headers: await headers()
+    // });
+    // if(!session?.user) { redirect('/') }
+    try {
+        const invitation = await prisma.organizationInvite.findFirst({
+            where: {
+                id: invitationId,
+            },
+            include: {
+                organization: true
+            }
+        })
+        if(!invitation) {
+            return {
+                error:true,
+                message:"Invitation Expired or doesn't exist"
+            }
+        }
+        // const userAlreadyMember = await prisma.organizationUser.findFirst({
+        //     where: {
+        //         userId: session?.user?.id,
+        //         organizationId: invitation.organizationId
+        //     }
+        // })
+        // if(userAlreadyMember) {
+        //     return {
+        //         error: true,
+        //         message:"You are already a member of this organization."
+        //     }
+        // }
+        return { 
+            error:false,
+            invitation: invitation
+         }
+    } catch (error) {
+        console.error(error);
+    }
+}
