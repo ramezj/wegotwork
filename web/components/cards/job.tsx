7dac33398405de0fa9398b7b740988b989a60cfd
Ramez Joseph <ramezjoseph8@gmail.com>
2025-07-18 10:37:09 +0300

    job card font bold

--- web/components/cards/job.tsx
+++ web/components/cards/job.tsx
@@ -26,7 +26,7 @@ export function JobCardForDashboard({ job }: { job: Job}) {
       <div onClick={handleCardClick}>
       <div className="w-full flex border border-foreground/20 border-dashed hover:border-white/30 bg-theme rounded-none items-center duration-200 pt-3 pb-3 cursor-pointer">
       <div className="mx-5 my-3 flex flex-col items-start text-left">
-        <p className='sm:text-lg text-md font-medium text-left text-white'>
+        <p className='sm:text-lg text-md font-bold text-left text-foreground'>
          {job.title}     
         </p>
         <div className="mt-1 flex">
@@ -48,7 +48,7 @@ export function JobCard({ job }: { job: JobWithCategories }) {
       <Link target="_blank" href={`/${job.id}`}>
       <div className="w-full bg-theme flex border border-foreground/20 border-dashed hover:border-white/30 duration-200 rounded-none items-center pt-3 pb-3 cursor-pointer">
       <div className="mx-5 my-3 flex flex-col items-start text-left">
-        <p className='sm:text-lg text-md font-medium text-left text-white'>
+        <p className='sm:text-lg text-md font-bold text-left text-foreground'>
          {job.title}     
         </p>
         <div className="mt-2 flex gap-2">
@@ -95,7 +95,7 @@ export function LandingPageJobCard({ title, type, location, age, category }: { t
   return (
       <div className="w-full bg-theme cursor-default flex border border-foreground/20 border-dashed duration-200 rounded-none items-center pt-3 pb-3">
       <div className="mx-5 my-3 flex flex-col items-start text-left">
-      <p className='sm:text-lg text-md font-medium text-left text-white'>
+      <p className='sm:text-lg text-md font-bold text-left text-foreground'>
        {title}     
       </p>
       <div className="mt-2 flex gap-2">
@@ -129,7 +129,7 @@ export function JobCardForApplicants({ job, applicants }: { job: Job, applicants
     <div onClick={handleCardClick}>
     <div className="w-full bg-theme flex border border-foreground/20 border-dashed hover:border-white/30 duration-200 rounded-none items-center pt-3 pb-3 cursor-pointer">
     <div className="mx-5 my-3 flex flex-col items-start text-left">
-      <p className='sm:text-lg text-md font-medium text-left text-white'>
+      <p className='sm:text-lg text-md font-bold text-left text-foreground'>
        {job.title}     
       </p>
       <div className="mt-1 flex">
