26c590028d2741d2689bc1f13ff7081e32218ab3
ramezj <ramezjoseph8@gmail.com>
2025-12-05 10:32:26 +0200

    button, ui, components, features,

--- apps/app/page.tsx
+++ apps/app/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
       <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
         Hello World.
       </h1>
+      <Button>Click me</Button>
     </div>
   );
 }
