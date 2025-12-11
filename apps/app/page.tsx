41457f155f4c0a46b571bbdf230b0a9c22e9f148
ramezj <ramezjoseph8@gmail.com>
2025-12-12 00:39:33 +0200

    implementing the navigation bar.

--- apps/app/page.tsx
+++ apps/app/page.tsx
@@ -1,12 +1,17 @@
 import { Button } from "@/components/ui/button";
+import { NavigationBar } from "@/components/shared/navigation-bar";
 
 export default function Home() {
   return (
     <div>
-      <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-        Hello World.
-      </h1>
-      <Button>Click me</Button>
+      <div className="min-h-screen text-white bg-red-500 max-w-screen">
+        <NavigationBar />
+        <h1 className="max-w-xs text-3xl leading-10 tracking-tight text-black dark:text-zinc-50 font-bold">
+          wegotwork
+        </h1>
+        <Button>Click me</Button>
+      </div>
+      <div>hello world!</div>
     </div>
   );
 }
