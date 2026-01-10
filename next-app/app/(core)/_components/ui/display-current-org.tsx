1a5ed2c3394d354acbe69dafb2563409ab38aeac
ramezj <ramezjoseph8@gmail.com>
2026-01-10 03:17:28 +0200

    switching from next to tanstack! finishing finals in 2 days.

--- next-app/app/(core)/_components/ui/display-current-org.tsx
+++ next-app/app/(core)/_components/ui/display-current-org.tsx
@@ -14,9 +14,10 @@ export default function DisplayCurrentOrganization() {
   });
 
   return (
-    <>
-      <p>Current Organization: {organization?.name}</p>
-      <p>Current Organization slug: {organization?.slug}</p>
+    <main className="space-y-4">
+      <p className="text-muted-foreground">
+        Welcome back! Here is what's happening with {organization?.name} today.
+      </p>
       <div className="flex xl:flex-row flex-col w-full gap-4">
         <AnalyticsCard
           cardTitle="Total Jobs"
@@ -24,12 +25,12 @@ export default function DisplayCurrentOrganization() {
           cardNumber={1}
         />
         <AnalyticsCard
-          cardTitle="Total Jobs"
+          cardTitle="Total Applicants"
           cardContent={organization?.jobs.length}
           cardNumber={1}
         />
         <AnalyticsCard
-          cardTitle="Total Jobs"
+          cardTitle="Total Categories"
           cardContent={organization?.jobs.length}
           cardNumber={1}
         />
@@ -39,6 +40,6 @@ export default function DisplayCurrentOrganization() {
           cardNumber={1}
         />
       </div>
-    </>
+    </main>
   );
 }
--- platform/app/auth/page.tsx
+++
@@ -1,5 +0,0 @@
-import { LoginButton } from "@/components/shared/auth/login-button";
-
-export default async function Page() {
-  return <LoginButton />;
-}
