9e92ffe709d4415675ba276a0c34350177b25c2c
ramezj <ramezjoseph8@gmail.com>
2026-01-21 15:18:11 +0200

    initiated prisma with a postgres database in railway.

--- www/src/lib/auth.ts
+++ www/src/lib/auth.ts
@@ -1,6 +1,11 @@
 import { betterAuth } from "better-auth";
+import prisma from "./prisma";
+import { prismaAdapter } from "better-auth/adapters/prisma";
 
 export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
   socialProviders: {
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID!,
