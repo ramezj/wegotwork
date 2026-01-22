f790ec3ddbfcd0ac05718ca4de05b7ec9f0f0fa3
ramezj <ramezjoseph8@gmail.com>
2026-01-22 11:06:27 +0200

    create-organization done!

--- www/src/components/organization/create-organization.tsx
+++ www/src/components/organization/create-organization.tsx
@@ -1,3 +1,92 @@
+import { Button } from "../ui/button";
+import { Dialog, DialogTrigger, DialogContent } from "../ui/dialog";
+import {
+  Field,
+  FieldDescription,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+  FieldLegend,
+  FieldSet,
+} from "../ui/field";
+import { useForm, Controller } from "react-hook-form";
+import { createOrganizationSchema } from "@/server/organization/schemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import z from "zod";
+import { Input } from "../ui/input";
+import { useMutation } from "@tanstack/react-query";
+import { createOrganizationFn } from "@/server/organization/create-organization";
+import { Loader } from "lucide-react";
+
 export function CreateOrganization() {
-  return <></>;
+  const form = useForm<z.infer<typeof createOrganizationSchema>>({
+    resolver: zodResolver(createOrganizationSchema),
+    defaultValues: {
+      name: "",
+      slug: "",
+    },
+    mode: "onSubmit",
+  });
+  const mutation = useMutation({
+    mutationFn: createOrganizationFn,
+    onSuccess: async () => {
+      console.log("Done!");
+    },
+  });
+  const onSubmit = async (data: z.infer<typeof createOrganizationSchema>) => {
+    mutation.mutateAsync({ data });
+  };
+  return (
+    <>
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button>Create New Organization</Button>
+        </DialogTrigger>
+        <DialogContent>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FieldGroup>
+              <FieldSet>
+                <FieldLegend>Create Organization</FieldLegend>
+                <FieldDescription>
+                  Create an organization and start hiring
+                </FieldDescription>
+                <FieldGroup>
+                  <Controller
+                    control={form.control}
+                    name="name"
+                    render={({ field, fieldState }) => (
+                      <Field data-invalid={fieldState.invalid}>
+                        <FieldLabel htmlFor={field.name}>Name</FieldLabel>
+                        <Input {...field} placeholder="Name" />
+                        {fieldState.error && (
+                          <FieldError errors={[fieldState.error]} />
+                        )}
+                      </Field>
+                    )}
+                  />
+                  <Controller
+                    control={form.control}
+                    name="slug"
+                    render={({ field, fieldState }) => (
+                      <Field data-invalid={fieldState.invalid}>
+                        <FieldLabel htmlFor={field.name}>Name</FieldLabel>
+                        <Input {...field} placeholder="Slug" />
+                        {fieldState.error && (
+                          <FieldError errors={[fieldState.error]} />
+                        )}
+                      </Field>
+                    )}
+                  />
+                  <Button disabled={mutation.isPending} type="submit">
+                    {mutation.isPending && <Loader className="animate-spin" />}
+                    Create
+                  </Button>
+                </FieldGroup>
+              </FieldSet>
+            </FieldGroup>
+          </form>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
 }
--- www/src/routes/dashboard/organizations/route.tsx
+++ www/src/routes/dashboard/organizations/route.tsx
@@ -1,6 +1,7 @@
 import { createFileRoute, redirect } from "@tanstack/react-router";
 import { authClient } from "@/lib/auth-client";
 import Header from "@/components/shared/header";
+import { CreateOrganization } from "@/components/organization/create-organization";
 
 export const Route = createFileRoute("/dashboard/organizations")({
   component: RouteComponent,
@@ -18,6 +19,7 @@ function RouteComponent() {
       <main className="p-4 items-center content-center justify-center text-center">
         <h1 className="text-2xl">Your Organizations</h1>
       </main>
+      <CreateOrganization />
     </div>
   );
 }
