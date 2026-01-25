5825868fe02e2a2bea2210bd23d18a41ec642d3d
ramezj <ramezjoseph8@gmail.com>
2026-01-25 14:47:26 +0200

    removed unused components.

--- www/src/routes/$slug/_layout/index.tsx
+++ www/src/routes/$slug/_layout/index.tsx
@@ -1,5 +1,10 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { DashboardContent } from "@/components/dashboard/dashboard-content";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { createFileRoute, Navigate } from "@tanstack/react-router";
+import { getOrganizationBySlugFn } from "@/server/organization/get-by-slug";
+import { StatisticCard } from "@/components/dashboard/statistics";
+import { Briefcase, Users } from "lucide-react";
+import { JobCard } from "@/components/job/job-card";
+import { Button } from "@/components/ui/button";
 
 export const Route = createFileRoute("/$slug/_layout/")({
   component: RouteComponent,
@@ -9,5 +14,54 @@ export const Route = createFileRoute("/$slug/_layout/")({
 });
 
 function RouteComponent() {
-  return <DashboardContent />;
+  const { session } = Route.useRouteContext();
+  const { slug } = Route.useParams();
+
+  const { data } = useSuspenseQuery({
+    queryKey: ["organization", slug],
+    queryFn: () => getOrganizationBySlugFn({ data: { slug } }),
+    staleTime: 60 * 60 * 1000,
+  });
+
+  if (!data?.organization) {
+    return <Navigate to="/dashboard" replace />;
+  }
+
+  return (
+    <div className="space-y-4">
+      <div className="flex items-center justify-between">
+        <h1 className="text-xl">
+          Welcome back, <b>{session.user.name}</b>
+        </h1>
+        <Button>Preview</Button>
+      </div>
+      <div className="flex lg:flex-row flex-col gap-4">
+        <StatisticCard
+          title="Organization"
+          amount={data?.organization?.name || ""}
+          icon={<Briefcase className="size-4" />}
+        />
+        <StatisticCard
+          title="Jobs"
+          amount={data?.organization?.jobs?.length || 0}
+          icon={<Briefcase className="size-4" />}
+        />
+        <StatisticCard
+          title="Categories"
+          amount={data?.organization?.categories?.length || 0}
+          icon={<Users className="size-4" />}
+        />
+      </div>
+      <div className="space-y-4">
+        <h1 className="text-xl">
+          Your Job Openings <b>({data?.organization?.jobs?.length || 0})</b>
+        </h1>
+        <div className="flex flex-col space-y-4">
+          {data?.organization?.jobs?.map((job) => (
+            <JobCard key={job.id} job={job} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
--- www/src/routes/$slug/_layout/jobs/route.tsx
+++ www/src/routes/$slug/_layout/jobs/route.tsx
@@ -1,13 +1,39 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { Loader } from "lucide-react";
-
-import { Suspense } from "react";
-import { JobsContent } from "@/components/job/jobs-content";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { createFileRoute, Navigate } from "@tanstack/react-router";
+import { getOrganizationBySlugFn } from "@/server/organization/get-by-slug";
+import { JobCard } from "@/components/job/job-card";
+import { Button } from "@/components/ui/button";
 
 export const Route = createFileRoute("/$slug/_layout/jobs")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <JobsContent />;
+  const { slug } = Route.useParams();
+
+  const { data } = useSuspenseQuery({
+    queryKey: ["organization", slug],
+    queryFn: () => getOrganizationBySlugFn({ data: { slug } }),
+    staleTime: 60 * 60 * 1000,
+  });
+
+  if (!data?.organization) {
+    return <Navigate to="/dashboard" replace />;
+  }
+
+  return (
+    <div className="space-y-4">
+      <div className="flex items-center justify-between">
+        <h1 className="text-xl">
+          Job Openings <b>({data?.organization?.jobs?.length || 0})</b>
+        </h1>
+        <Button>Create</Button>
+      </div>
+      <div className="flex flex-col space-y-4">
+        {data?.organization?.jobs?.map((job) => (
+          <JobCard key={job.id} job={job} />
+        ))}
+      </div>
+    </div>
+  );
 }
