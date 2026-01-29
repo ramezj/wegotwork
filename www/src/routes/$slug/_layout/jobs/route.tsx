81ee357bacf2029176c1f7a41b86155325795258
ramezj <ramezjoseph8@gmail.com>
2026-01-29 09:16:59 +0200

    fixed the flashing of /$jobId

--- www/src/routes/$slug/_layout/jobs/route.tsx
+++ www/src/routes/$slug/_layout/jobs/route.tsx
@@ -1,9 +1,15 @@
+import { LoadingLayout } from "@/components/shared/layout";
 import { Outlet, createFileRoute } from "@tanstack/react-router";
+import { Suspense } from "react";
 
 export const Route = createFileRoute("/$slug/_layout/jobs")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <Outlet />;
+  return (
+    <Suspense fallback={<LoadingLayout title="" />}>
+      <Outlet />
+    </Suspense>
+  );
 }
