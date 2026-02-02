cd01b856a6222c8d29abfde3b1bc52d227f6b6d9
ramezj <ramezjoseph8@gmail.com>
2026-02-02 12:28:41 +0200

    ..

--- www/src/components/auth/auth-buttons.tsx
+++ www/src/components/auth/auth-buttons.tsx
@@ -4,6 +4,7 @@ import { Button } from "../ui/button";
 export function SignInButton() {
   return (
     <Button
+      className="w-52"
       onClick={() => {
         authClient.signIn.social({ provider: "google", callbackURL: "/" });
       }}
--- www/src/components/shared/header.tsx
+++ www/src/components/shared/header.tsx
@@ -2,8 +2,23 @@ import { Session } from "@/features/auth/auth";
 import { SignInButton } from "../auth/auth-buttons";
 import { Link } from "@tanstack/react-router";
 import { Button } from "../ui/button";
-import { Menu } from "lucide-react";
-import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
+import { Book, Menu, Sunset, Trees, X, Zap } from "lucide-react";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "../ui/sheet";
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "../ui/accordion";
+import React from "react";
+import { cn } from "@/lib/utils";
 
 export default function Header({
   session,
@@ -12,24 +27,36 @@ export default function Header({
   session: Session | null;
   // isPending: boolean;
 }) {
+  const [isOpen, setIsOpen] = React.useState(false);
+  React.useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "unset";
+    }
+    return () => {
+      document.body.style.overflow = "unset";
+    };
+  }, [isOpen]);
   return (
     <header className="w-full sticky top-0 z-50 h-16 border border-b border-l-0 border-r-0 bg-background flex flex-row items-center justify-between">
       <div className="flex-row flex gap-2 items-center content-center align-middle">
         <Link to={"/"}>
           <h1 className="font-bold pl-4">LOOP</h1>
         </Link>
+        <div className="flex-row gap-2 items-center content-center align-middle md:flex hidden">
+          <Link className="font-medium text-gray-200" to={"/"}>
+            home
+          </Link>
+          <Link className="font-medium text-gray-200" to={"/"}>
+            support
+          </Link>
+          <Link className="font-medium text-gray-200" to={"/"}>
+            blog
+          </Link>
+        </div>
       </div>
       <div className="flex-row gap-2 items-center md:flex hidden">
-        <Link className="font-medium" to={"/"}>
-          home
-        </Link>
-        <Link className="font-medium" to={"/"}>
-          support
-        </Link>
-        <Link className="font-medium" to={"/"}>
-          blog
-        </Link>
-        {/* {isPending && <SignInButton />} */}
         {session?.user ? (
           <>
             <Button asChild className="h-16 px-8 font-semibold">
@@ -37,22 +64,54 @@ export default function Header({
             </Button>
           </>
         ) : (
-          // !isPending && (
           <>
             <SignInButton />
           </>
-          // )
         )}
       </div>
       <div className="md:hidden flex">
-        <Sheet>
-          <SheetTrigger asChild>
-            <Button className="h-16 px-6!">
-              <Menu />
-            </Button>
-          </SheetTrigger>
-          <SheetContent side="right">Hello World</SheetContent>
-        </Sheet>
+        <Button
+          variant="ghost"
+          size="icon"
+          className="md:hidden z-50 text-white hover:text-white px-8 h-16 bg-inherit hover:bg-inherit rounded-none"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          {isOpen ? <X className="h-8 w-8" /> : <Menu className="h-8 w-8" />}
+        </Button>
+      </div>
+      <div
+        className={cn(
+          "fixed inset-0 bg-black z-40 flex flex-col items-center justify-center transition-all duration-300 ease-in-out md:hidden",
+          isOpen
+            ? "opacity-100 translate-y-0"
+            : "opacity-0 -translate-y-full pointer-events-none",
+        )}
+      >
+        <nav className="flex flex-col items-center space-y-6 text-center">
+          {["Features", "Demo", "Pricing"].map((item) => (
+            <Link
+              key={item}
+              to={`/`}
+              className="text-2xl text-white transition-colors font-bold"
+              onClick={() => setIsOpen(false)}
+            >
+              {item}
+            </Link>
+          ))}
+          {session?.user ? (
+            <>
+              <Button asChild className="w-52">
+                <Link to="/dashboard" onClick={() => setIsOpen(false)}>
+                  Dashboard
+                </Link>
+              </Button>
+            </>
+          ) : (
+            <>
+              <SignInButton />
+            </>
+          )}
+        </nav>
       </div>
     </header>
   );
