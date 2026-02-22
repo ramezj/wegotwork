534da74f2aeb195f47a0d36d2f0fedd3c954368b
ramezj <ramezjoseph8@gmail.com>
2026-02-22 22:16:50 +0200

    increased height from 28 to 30

--- www/src/components/job/job-card.tsx
+++ www/src/components/job/job-card.tsx
@@ -1,5 +1,5 @@
 import { Card } from "../ui/card";
-import { ArrowRight, ArrowUpRight } from "lucide-react";
+import { ArrowRight } from "lucide-react";
 import { Link } from "@tanstack/react-router";
 import { JobWithCategory } from "@/types/job/job";
 import { Badge } from "../ui/badge";
@@ -37,7 +37,7 @@ export function JobCard({
       to={isDemo ? "/" : "/$slug/jobs/$jobId"}
       params={{ slug, jobId: job.id }}
     >
-      <Card className="w-full group min-h-28 bg-input/30 hover:bg-input/40 transition-all flex flex-row border border-input rounded-none items-center p-5 cursor-pointer shadow-none gap-0">
+      <Card className="w-full group min-h-30 bg-input/30 hover:bg-input/40 transition-all flex flex-row border border-input rounded-none items-center p-5 cursor-pointer shadow-none gap-0">
         <div className="flex flex-1 flex-col">
           <div className="flex items-center justify-between">
             {/* job information */}
@@ -86,7 +86,7 @@ export function JobCardForViewPage({
       to={"/view/$slug/$jobId"}
       params={{ slug, jobId: job.id }}
     >
-      <Card className="w-full group min-h-28 bg-input/30 hover:bg-input/40 transition-all flex flex-row border border-input rounded-none items-center p-5 cursor-pointer shadow-none gap-0">
+      <Card className="w-full group min-h-30 bg-input/30 hover:bg-input/40 transition-all flex flex-row border border-input rounded-none items-center p-5 cursor-pointer shadow-none gap-0">
         <div className="flex flex-1 flex-col">
           <div className="flex items-center justify-between">
             {/* job information */}
