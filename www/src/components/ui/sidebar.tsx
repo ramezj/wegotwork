dae483548e9ebc844ee9b32209235ad872f21833
Ramez Joseph <ramezjoseph8@gmail.com>
2026-03-22 01:12:16 +0200

    Revert "...."
    This reverts commit 0f69ca1846a06cf5835f7cae36fde8fc90edfd92.

--- www/src/components/ui/sidebar.tsx
+++ www/src/components/ui/sidebar.tsx
@@ -187,7 +187,7 @@ function Sidebar({
           data-sidebar="sidebar"
           data-slot="sidebar"
           data-mobile="true"
-          className="bg-sidebar text-sidebar-foreground h-dvh max-h-dvh w-(--sidebar-width) gap-0 overflow-hidden p-0 [&>button]:hidden"
+          className="bg-sidebar text-sidebar-foreground w-(--sidebar-width) p-0 [&>button]:hidden"
           style={
             {
               "--sidebar-width": SIDEBAR_WIDTH_MOBILE,
@@ -199,7 +199,7 @@ function Sidebar({
             <SheetTitle>Sidebar</SheetTitle>
             <SheetDescription>Displays the mobile sidebar.</SheetDescription>
           </SheetHeader>
-          <div className="flex min-h-0 flex-1 w-full flex-col">{children}</div>
+          <div className="flex h-full w-full flex-col">{children}</div>
         </SheetContent>
       </Sheet>
     );
